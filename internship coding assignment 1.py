654b37e103b6bb6e8a7523517b54ce8be8d3ffb2
sanikakumar <96332436+sanikakumar@users.noreply.github.com>
2021-12-20 19:18:23 +0800

    Add files via upload

---
+++ internship coding assignment 1.py
@@ -0,0 +1,27 @@
+import fitz
+import xlwt
+
+def open_file(fname):
+
+    doc = fitz.open(fname) 
+    page = doc.load_page(11)
+    text = page.get_text("blocks")
+   #  put close file 
+    
+    my_xls = xlwt.Workbook(encoding='ascii', style_compression=0)
+    my_sheet = my_xls.add_sheet("Paragraphs-Sheet", cell_overwrite_ok=True)
+    
+    row_no = 0
+    
+    for paragraph in text:
+        
+        if (paragraph[6] == 0):
+            my_sheet.write(row_no,0,paragraph[4])
+            row_no+=1
+            
+    my_xls.save(r"C:\Users\home\Documents\Python-Programs\results.xls")
+    
+    return()
+
+
+open_file(r"C:\Users\home\Documents\Python-Programs\keppel.pdf")
